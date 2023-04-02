9c9703bb662cbe37d79333b5ad258103dc709d6e
KisChang <734615869@qq.com>
2023-04-02 18:58:46 +0800

    Update micropython.mk
    fix .mk issue

--- picowota_reboot/micropython.mk
+++ picowota_reboot/micropython.mk
@@ -2,6 +2,7 @@ EXAMPLE_MOD_DIR := $(USERMOD_DIR)
 
 # Add all C files to SRC_USERMOD.
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/reboot.c
+SRC_USERMOD += $(EXAMPLE_MOD_DIR)/reboot_mp.c
 
 # We can add our module folder to include paths if needed
 # This is not actually needed in this example.
